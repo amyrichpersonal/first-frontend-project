3328296aa44ec1fc3522a079bd563aa69d66cb8a
amyrichpersonal <richardsona14197@gmail.com>
2022-11-01 23:25:48 +0000

    first commit of first component

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './HelloWorld.js'
+import HelloWorld from './HelloWorld.js';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
